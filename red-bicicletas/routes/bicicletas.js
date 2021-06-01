4ec6b2719c5275954201978ee1ba1afb06e8374e
davidssantoss <davidsteven.santos9@gmail.com>
2021-06-01 12:40:28 -0500

    Añadiendo CRUD al proyecto sin DB

---
+++ red-bicicletas/routes/bicicletas.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var bicicleta_controller = require('../controllers/bicicleta');
+
+router.get('/', bicicleta_controller.bicicleta_list);
+
+router.get('/create', bicicleta_controller.bicicleta_create_get);
+router.post('/create', bicicleta_controller.bicicleta_create_post);
+
+router.get('/:id/update', bicicleta_controller.bicicleta_update_get);
+router.post('/:id/update', bicicleta_controller.bicicleta_update_post);
+router.post('/:id/delete', bicicleta_controller.bicicleta_delete_post);
+
+
+module.exports = router;
